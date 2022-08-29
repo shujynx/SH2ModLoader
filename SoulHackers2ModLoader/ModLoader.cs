724402921f1880eb47599e2da2953285ba37e0a2
Shujynx <82668939+shujynx@users.noreply.github.com>
2022-08-29 20:46:06 +0200

    Added source files

---
+++ SoulHackers2ModLoader/ModLoader.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using Il2CppSystem.IO;
+using UnityEngine;
+
+namespace SoulHackers2ModLoader;
+
+[HarmonyPatch]
+internal class ModLoader
+{
+    private static readonly ManualLogSource _fileLog = BepInEx.Logging.Logger.CreateLogSource("FileLog");
+
+    [HarmonyPatch(typeof(AssetBundle), nameof(AssetBundle.LoadFromFile), typeof(string))]
+    [HarmonyPrefix]
+    private static void LoadFromFile(ref string path)
+    {
+        path = ModFile(path);
+        if (Plugin.Instance.verboseMode.Value) LogFile(path);
+    }
+
+    [HarmonyPatch(typeof(AssetBundle), nameof(AssetBundle.LoadFromFileAsync), typeof(string))]
+    [HarmonyPrefix]
+    private static void LoadFromFileAsync(ref string path)
+    {
+        path = ModFile(path);
+        if (Plugin.Instance.verboseMode.Value) LogFile(path);
+    }
+
+    private static void LogFile(string path) => _fileLog.LogInfo(path.Replace(Application.streamingAssetsPath, ""));
+
+    private static string ModFile(string path)
+    {
+        string modPath = path.Replace("/SOUL HACKERS2_Data/StreamingAssets/", "/mods/");
+        if (File.Exists(modPath)) path = modPath;
+        return path;
+    }
+}
---
+++ SoulHackers2ModLoader/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppSystem.IO;
+using UnityEngine;
+
+namespace SoulHackers2ModLoader;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("SOUL HACKERS2.exe")]
+public class Plugin : BasePlugin
+{
+    public static Plugin Instance { get; private set; }
+    public new static ManualLogSource Log { get; private set; }
+    public ConfigEntry<bool> verboseMode { get; set; }
+    public override void Load()
+    {
+
+        Instance = this;
+        Log = base.Log;
+
+        Log.LogInfo($"{MyPluginInfo.PLUGIN_NAME} is loaded!");
+        string modsPath = Application.dataPath.Replace("/SOUL HACKERS2_Data", "/mods");
+        if (!Directory.Exists(modsPath)) 
+        {
+            Directory.CreateDirectory(modsPath);
+            Log.LogWarning("Mods folder didn't exist. Created one!");
+        }
+
+        InitConfig();
+
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+    }
+
+    private void InitConfig()
+    {
+        Config.SaveOnConfigSet = true;
+
+        verboseMode = Config.Bind(
+            "Debug",
+            "Verbose Mode",
+            false,
+            "Prints out the path to what file is currently being loaded");
+    }
+}
