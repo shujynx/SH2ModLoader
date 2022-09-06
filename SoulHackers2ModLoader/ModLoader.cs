5173655c14737f219a8ea1bc2f63a153a5d16331
Shujynx <82668939+shujynx@users.noreply.github.com>
2022-09-06 18:08:40 +0200

    Added usm loading

--- SoulHackers2ModLoader/ModLoader.cs
+++ SoulHackers2ModLoader/ModLoader.cs
@@ -1,5 +1,6 @@
 ﻿using AtLib.AtSound;
 using BepInEx.Logging;
+using Cpp2IL.Core.Analysis.Actions.x86;
 using HarmonyLib;
 using Il2CppSystem.IO;
 using UnityEngine;
@@ -32,11 +33,17 @@ internal class ModLoader
     [HarmonyPrefix]
     private static void LoadPack(ref string name, bool readStream, UnityAction completeMethod)
     {
-        string moddedAudio = "mods/" + name;
-        if (File.Exists(Application.streamingAssetsPath + "/" + moddedAudio + ".awb") && File.Exists(Application.streamingAssetsPath + "/" + moddedAudio + ".acb")) name = moddedAudio;
+        name = ModAudio(name);
         if (Plugin.Instance.verboseMode.Value) LogFile("/" + name);
     }
-    
+
+    [HarmonyPatch(typeof(UIMoviePlayer), nameof(UIMoviePlayer.Prepare))]
+    [HarmonyPrefix]
+    private static void MovieFunction(ref string moviePath)
+    {
+        moviePath = ModMovie(moviePath);
+        if (Plugin.Instance.verboseMode.Value) LogFile(moviePath);
+    }
     private static void LogFile(string path) => _fileLog.LogInfo(path.Replace(Application.streamingAssetsPath, ""));
 
     private static string ModFile(string path)
@@ -44,5 +51,19 @@ internal class ModLoader
         string modPath = path.Replace("/SOUL HACKERS2_Data/StreamingAssets/", "/SOUL HACKERS2_Data/StreamingAssets/mods/");
         if (File.Exists(modPath)) path = modPath;
         return path;
+    }
+
+    private static string ModAudio(string name)
+    {
+        string moddedAudio = "mods/" + name;
+        if (File.Exists(Application.streamingAssetsPath + "/" + moddedAudio + ".awb") && File.Exists(Application.streamingAssetsPath + "/" + moddedAudio + ".acb")) name = moddedAudio;
+        return name;
+    }
+
+    private static string ModMovie(string moviePath)
+    {
+        string moddedMovie = moviePath.Replace("/SOUL HACKERS2_Data/StreamingAssets/Movie/", "/SOUL HACKERS2_Data/StreamingAssets/mods/Movie/");
+        if (File.Exists(moddedMovie)) moviePath = moddedMovie;
+        return moviePath;
     }
 }
